85db7cc6ef8d19f9c805952603719da3c6c2a8c2
Ai Sato <ahfmrs1019@gmail.com>
2023-12-31 00:05:36 +0900

    create homepage

--- resources/views/contents/home.blade.php
+++ resources/views/contents/home.blade.php
@@ -1,6 +1,12 @@
 @extends('App')
 
-@section('title','home')
+@section('title', 'home')
 
 @section('contents')
 @endsection
+
+<div class="d-flex align-items-start bg-body-tertiary mb-3" style="height: 100px;">
+    <div class="col">One of three columns</div>
+    <div class="col">One of three columns</div>
+    <div class="col">One of three columns</div>
+</div>
