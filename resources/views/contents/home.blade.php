772620d2979ee33c927b9e4b15a89e5471d16019
Ai Sato <ahfmrs1019@gmail.com>
2023-12-31 07:42:58 +0900

    Created a sidebar for the homepage

--- resources/views/contents/home.blade.php
+++ resources/views/contents/home.blade.php
@@ -5,8 +5,63 @@
 @section('contents')
 @endsection
 
-<div class="d-flex align-items-start bg-body-tertiary mb-3" style="height: 100px;">
-    <div class="col">One of three columns</div>
-    <div class="col">One of three columns</div>
+<div class="d-flex align-items-start bg-body-tertiary mb-3" style="height: 100vh;">
+    <div class="col border-end h-100">
+        <div class="d-flex flex-column bg-light flex-shrink-0 p-3" style="width: 280px;">
+            <a href="/" class="d-flex align-items-center mb-md-0 me-md-auto link-dark text-decoration-none mb-3">
+                <svg class="bi me-2" width="40" height="32">
+                    <use xlink:href="#bootstrap"></use>
+                </svg>
+                <span class="fs-4">Money logger</span>
+            </a>
+            <hr>
+            <ul class="nav nav-pills flex-column mb-auto">
+                <li class="nav-item">
+                    <a href="#" class="nav-link active" aria-current="page">
+                        <svg class="bi me-2" width="16" height="16">
+                            <use xlink:href="#home"></use>
+                        </svg>
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link link-dark">
+                        <svg class="bi me-2" width="16" height="16">
+                            <use xlink:href="#speedometer2"></use>
+                        </svg>
+                        Income
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link link-dark">
+                        <svg class="bi me-2" width="16" height="16">
+                            <use xlink:href="#table"></use>
+                        </svg>
+                        Orders
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link link-dark">
+                        <svg class="bi me-2" width="16" height="16">
+                            <use xlink:href="#grid"></use>
+                        </svg>
+                        Products
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link link-dark">
+                        <svg class="bi me-2" width="16" height="16">
+                            <use xlink:href="#people-circle"></use>
+                        </svg>
+                        Customers
+                    </a>
+                </li>
+            </ul>
+            <hr>
+
+
+        </div>
+    </div>
+    <div class="col border-end h-100">One of three columns</div>
     <div class="col">One of three columns</div>
 </div>
