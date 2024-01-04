9c0827e10dfdd279449854777f7e20f4b04f5cd1
Ai Sato <ahfmrs1019@gmail.com>
2024-01-05 01:49:18 +0900

    create record controller

--- app/Http/Controllers/record/CreateRecordController.php
+++ app/Http/Controllers/record/CreateRecordController.php
@@ -5,11 +5,38 @@ declare(strict_types=1);
 namespace App\Http\Controllers\record;
 
 use App\Http\Controllers\Controller as ControllersController;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class CreateRecordController extends ControllersController
 {
     public function index()
     {
         return view('record.money-records');
+    }
+
+    public function handle(Request $http_request)
+    {
+        $validate = $http_request->validate(
+            [
+                'money_amount' => ['required', 'numeric'],
+            ]
+        );
+
+        try {
+            $record = Record::create(
+                [
+                    'record_id' => (string)Str::uuid(),
+                    'money_amount' => $validate['money_amount'],
+                    'user_id' => Auth::user()->user_id,
+                ]
+            );
+        } catch (Exception $exception) {
+            Log::error(report($exception));
+            return redirect()->back();
+        }
+
+        return view('record.record-complete');
     }
 }
